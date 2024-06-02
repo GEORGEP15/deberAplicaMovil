d56de05081cb41e9db08c120329f7327bf0730fc
GEORGE PEÑAFIEL <george.penafiela@ug.edu.ec>
2024-06-02 15:21:05 -0500

    Fin sección 2

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,39 +1,62 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { ComponentsModule } from '../components/components.module';
- 
+
 //crear una interface para definir propiedades que el componente lista va 
- 
-interface Componente { 
-  icon: string; 
-  name: string; 
-  redirectTo: string; 
-} 
- 
-@Component({ 
-  selector: 'app-inicio', 
-  templateUrl: './inicio.page.html', 
-  styleUrls: ['./inicio.page.scss'], 
-}) 
-export class InicioPage implements OnInit { 
- 
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
   //crear nuevo listado 
- 
+
   //componentes: any[] = []; 
-  componentes: Componente[] = [ 
-    { 
-      icon: 'american-football-outline', 
-      name: 'Action Sheet', 
-      redirectTo: '/action-sheet' 
-    }, 
-    { 
-      icon: 'logo-apple-appstore', 
-      name: 'Alert', 
-      redirectTo: '/alert' 
-    }, 
-  ]; 
- 
-  constructor() { } 
-  ngOnInit() { 
-  } 
- 
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+
+    //Se agregaron los suiguientes Componentes-iconos" en https://ionic.io/ionicons?_gl=1*k1jajn*_ga*MTgyNTE0ODk3MC4xNzE3MzQ0NjYx*_ga_REH9TJF6KF*MTcxNzM1MTg2MC4yLjEuMTcxNzM1MjMxMi4wLjAuMA..
+
+    {
+      icon: 'person-circle-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar'
+    },
+    {
+      icon: 'time-outline',
+      name: 'Datetime',
+      redirectTo: '/datetime'
+    },
+    {
+      icon: 'enter-outline',
+      name: 'Input',
+      redirectTo: '/input'
+    },
+
+
+
+
+
+  ];
+
+  constructor() { }
+  ngOnInit() {
+  }
+
 } 
